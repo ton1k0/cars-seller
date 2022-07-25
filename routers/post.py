be90843a9122c6d247005e0646a9bd8868624489
Alexandr <domarevsasha@gmail.com>
2022-07-25 13:42:46 +0300

    searching

--- routers/post.py
+++ routers/post.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+
 import models, schemas, oaut2
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from database import get_db
 from scripts import post
 
@@ -29,3 +30,13 @@ def destroy(id:int, db: Session = Depends(get_db), current_user:schemas.User = D
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id:int, request: schemas.Post, db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
     return post.update(id, request, db)
+
+
+@router.get('/search')
+def search(brand: str, model: str | None = None, generation: str| None = None,
+           car_body: str | None = None, engine: str | None = None,
+           actuator: str | None = None, transmission: str | None = None, price_from: int | None = None,
+           price_to: int | None = None, year_of_release_from: int | None = None, year_of_release_to: int | None = None,
+           mileage_from: int | None = None, mileage_to: int | None = None, db: Session = Depends(get_db)):
+    return post.search(brand, model, generation,car_body,engine,actuator,transmission, price_from,price_to,
+                       year_of_release_from,year_of_release_to,mileage_from,mileage_to,db)
--- scripts/post.py
+++ scripts/post.py
@@ -1,6 +1,9 @@
+from sqlalchemy import and_, or_, not_
 from fastapi import HTTPException, status
+from pydantic.class_validators import Optional
 from sqlalchemy.orm import Session
 import models, schemas
+
 
 
 def get_all(db: Session):
@@ -37,3 +40,26 @@ def update(id:int, request:schemas.Post, db:Session):
     post.update(request.dict())
     db.commit()
     return 'updated'
+
+
+def search(brand: str, model: str, generation: str,car_body: str,
+           engine: str, actuator: str, transmission: str, price_from: int, price_to: int,
+           year_of_release_from: int, year_of_release_to: int, mileage_from: int, mileage_to: int,
+           db: Session, limit: int = 10, skip: int = 0):
+    post = db.query(models.Post).filter(and_(brand == None or models.Post.brand == brand,
+                                             model == None or models.Post.model.contains(model)),
+                                             generation == None or models.Post.generation == generation,
+                                             car_body == None or models.Post.car_body == car_body,
+                                             engine == None or models.Post.engine == engine,
+                                             actuator == None or models.Post.actuator == actuator,
+                                             transmission == None or models.Post.transmission == transmission,
+                                             price_from == None or models.Post.price >= price_from,
+                                             price_to == None or models.Post.price <= price_to,
+                                             year_of_release_from == None or models.Post.year_of_release >= year_of_release_from,
+                                             year_of_release_to == None or models.Post.year_of_release <= year_of_release_to,
+                                             mileage_from == None or models.Post.mileage >= mileage_from,
+                                             mileage_to == None or models.Post.mileage <= mileage_to).all()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'detail': f'Post with this filters is not found'})
+    return post[skip : skip + limit]
